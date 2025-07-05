537997fda719d58934fe368502fcccdb68e5963b
John Barbieri <joao.barbieri.ctr@brightside.com>
2025-07-05 19:56:06 -0300

    feat: add axios, react-query and config query client provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,17 +4,22 @@ import "./global.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { theme } from "./theme";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
-  <MantineProvider theme={theme}>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
-  </MantineProvider>
+  <QueryClientProvider client={queryClient}>
+    <MantineProvider theme={theme}>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </MantineProvider>
+  </QueryClientProvider>
 );
