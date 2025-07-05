1c07f82035a40815ffa0926c4e04f2e94f09ccb5
John Barbieri <joao.barbieri.ctr@brightside.com>
2025-07-05 15:15:25 -0300

    feat: refactor Search component to handle form submission and improve structure

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,5 +1,5 @@
 import StyledBox from "./StyledBox";
-import { Button, Group, Input, Radio, Text } from "@mantine/core";
+import { Button, Group, Input, Radio, Text, Box } from "@mantine/core";
 import styled from "styled-components";
 import { useIsMobile } from "../hooks/useIsMobile";
 import { useState } from "react";
@@ -17,7 +17,8 @@ export default function Search({ onResults }) {
   const [searchType, setSearchType] = useState("people");
   const [query, setQuery] = useState("");
 
-  function handleSearch() {
+  function handleSearch(e) {
+    if (e) e.preventDefault();
     const data =
       searchType === "people"
         ? swapiMock.people.filter((p) =>
@@ -31,25 +32,35 @@ export default function Search({ onResults }) {
 
   return (
     <StyledBox w={410} h={230}>
-      {isMobile ? (
-        <Text>What are you searching for?</Text>
-      ) : (
-        <StyledText>What are you searching for?</StyledText>
-      )}
-      <Radio.Group value={searchType} onChange={setSearchType}>
-        <Group gap="xl" mt="md" mb="md">
-          <Radio color="green.0" value="people" label="People" />
-          <Radio color="green.0" value="movies" label="Movies" />
-        </Group>
-      </Radio.Group>
-      <Input
-        placeholder="e.g. Chewbacca, Yoda, Boba Fett"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <Button fullWidth mt="auto" onClick={handleSearch}>
-        Search
-      </Button>
+      <Box
+        component="form"
+        onSubmit={handleSearch}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
+        }}
+      >
+        {isMobile ? (
+          <Text>What are you searching for?</Text>
+        ) : (
+          <StyledText>What are you searching for?</StyledText>
+        )}
+        <Radio.Group value={searchType} onChange={setSearchType}>
+          <Group gap="xl" mt="md" mb="md">
+            <Radio color="green.0" value="people" label="People" />
+            <Radio color="green.0" value="movies" label="Movies" />
+          </Group>
+        </Radio.Group>
+        <Input
+          placeholder="e.g. Chewbacca, Yoda, Boba Fett"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <Button fullWidth mt="auto" type="submit">
+          Search
+        </Button>
+      </Box>
     </StyledBox>
   );
 }
