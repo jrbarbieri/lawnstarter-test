7d54d41d5747139504ef9cafa870f2b6c1aeebc2
John Barbieri <joao.barbieri.ctr@brightside.com>
2025-07-05 15:06:39 -0300

    feat: refactor Results component layout to improve structure and readability

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Button, Divider, Flex, Text } from "@mantine/core";
+import { Container, Button, Divider, Flex, Text, Box } from "@mantine/core";
 import StyledBox from "./StyledBox";
 import { useNavigate } from "react-router-dom";
 
@@ -10,30 +10,34 @@ export default function Results({ data, onBack }) {
     <StyledBox ml={30} h={600} w={600}>
       <Text variant="title">Results</Text>
       <Divider />
-      {data.length > 0 ? (
-        data.map((character, idx) => {
-          const isLast = idx === data.length - 1;
-          return (
-            <React.Fragment key={idx}>
-              <Flex justify="space-between" align="center" mb="sm" mt="sm">
-                <Text variant="subtitle">
-                  {character?.name || character?.title}
-                </Text>
-                <Button onClick={() => navigate("/person")}>See Details</Button>
-              </Flex>
-              {!isLast && <Divider />}
-            </React.Fragment>
-          );
-        })
-      ) : (
-        <Flex justify="center" align="center" h="100%">
-          <Text align="center" c="lightGrey.0">
-            There are zero matches.
-            <br />
-            Use the form to search for People or Movies.
-          </Text>
-        </Flex>
-      )}
+      <Box mb="xl">
+        {data.length > 0 ? (
+          data.map((character, idx) => {
+            const isLast = idx === data.length - 1 && !onBack;
+            return (
+              <React.Fragment key={idx}>
+                <Flex justify="space-between" align="center" mb="sm" mt="sm">
+                  <Text variant="subtitle">
+                    {character?.name || character?.title}
+                  </Text>
+                  <Button onClick={() => navigate("/person")}>
+                    See Details
+                  </Button>
+                </Flex>
+                {!isLast && <Divider />}
+              </React.Fragment>
+            );
+          })
+        ) : (
+          <Flex justify="center" align="center" h="100%">
+            <Text align="center" c="lightGrey.0">
+              There are zero matches.
+              <br />
+              Use the form to search for People or Movies.
+            </Text>
+          </Flex>
+        )}
+      </Box>
       {onBack && (
         <Button onClick={onBack} mt="auto">
           Back to search
