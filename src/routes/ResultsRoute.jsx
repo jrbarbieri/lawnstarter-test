6831bd5d8e683066f9ed014987f8f71f5bff07c9
John Barbieri <joao.barbieri.ctr@brightside.com>
2025-07-05 19:12:41 -0300

    chore: remove duplicated code merged accidentaly

--- src/routes/ResultsRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import Results from "../components/Results";
-
-export default function ResultsRoute() {
-  const location = useLocation();
-  const navigate = useNavigate();
-  const results = location.state?.results || [];
-  return <Results data={results} onBack={() => navigate("/")} />;
-}
