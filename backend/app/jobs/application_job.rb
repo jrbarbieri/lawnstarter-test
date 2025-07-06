152db3cd31bd5159c4aeff94a92e307106985305
John Barbieri <joao.barbieri.ctr@brightside.com>
2025-07-06 20:58:19 -0300

    feat: add sidekiq-scheduler for job scheduling and configure Redis service in docker-compose

--- backend/app/jobs/application_job.rb
+++ backend/app/jobs/application_job.rb
@@ -1,3 +1,6 @@
+require "sidekiq"
+require "sidekiq-scheduler"
+
 class ApplicationJob < ActiveJob::Base
   # Automatically retry jobs that encountered a deadlock
   # retry_on ActiveRecord::Deadlocked
